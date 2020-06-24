b5ae5ddd8c4203543b21c0b198fa47efa460f73e
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-06-23 22:19:36 -0500

    Class static variables, params return value, ATM

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,16 +1,35 @@
 //bank_account.h
+#include<iostream>
+#include<vector>
+
+#ifndef BANK_ACCOUNT_H//header guards
+#define BANK_ACCOUNT_H
+
+enum transaction{ DEPOSIT=1, WITHDRAW=2, DISPLAY=3};
+enum class OPTION{DEPOSIT=1, WITHDRAW=2, DISPLAY=3, EXIT=4};//c++11 
+
 class BankAccount
 {
 
 public:
-    BankAccount(){ balance = 500;}
-    BankAccount(int b) : balance{b} { } //initializer list
+    BankAccount() : BankAccount(0){}//delegating constructor
+    BankAccount(int b) : balance{b} { bank_balance += balance; } //initializer list
     int get_balance() const {return balance;}//inline class function
     void deposit(int amount);
     void withdraw(int amount);
+    static int get_bank_balance(){return bank_balance;}
 private:
     int balance;//lock this variable
-
+    static int bank_balance;
 };
 
 
+BankAccount& get_account(int i);//free function
+
+void display_menu();
+
+int get_choice();
+
+void handle_transaction(BankAccount& account, int choice);
+
+#endif
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,3 +1,4 @@
+#include "atm.h"
 #include "bank_account.h"
 #include<iostream>
 
@@ -6,22 +7,18 @@ using std::cin;
 
 int main()
 {
-	BankAccount account(1000);//create an instance/variable of class in stack memory
+	BankAccount account = get_account(1);
+	cout<<"Account: "<<account.get_balance()<<"\n";
+	//ATM atm(account);
+	//atm.run();
+
+	BankAccount account1 = account;
+	cout<<"Account1: "<<account1.get_balance()<<"\n";
 	
-	int balance = account.get_balance();
-	cout<<"Balance: "<<balance<<"\n";
-
-	int deposit;
-	cout<<"Enter deposit amount: ";
-	cin>>deposit;
-	account.deposit(deposit);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
-
-	int withdraw;
-	cout<<"Enter withdraw amount: ";
-	cin>>withdraw;
-	account.withdraw(withdraw);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
+	account.deposit(50);
+	cout<<"Account: "<<account.get_balance()<<"\n";
+	
+	cout<<"Account1: "<<account1.get_balance()<<"\n";
 
 	return 0;
 }

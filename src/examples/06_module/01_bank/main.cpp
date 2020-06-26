85f58137b25501996ceeb4586b0ac39644dfa785
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-06-25 20:03:23 -0500

    bankaccount inheritance

--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,5 +1,7 @@
 #include "atm.h"
 #include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
 #include<iostream>
 
 using std::cout;
@@ -7,8 +9,21 @@ using std::cin;
 
 int main()
 {
+	CheckingAccount c(1000);
+	cout<<c;
+	CheckingAccount c1;
+	cout<<c1;
+
+	SavingsAccount s(100);
+	cout<<s;
+	cout<<s.get_balance()<<"\n";//derived class 
+	SavingsAccount s1;
+	cout<<s1;//base class balance
+	
+
 	//ATM atm;
 	//atm.run();
+
 	
 	/*BankAccount account;
 	BranchBank bank(10000);

980a5dd97460bb2c7d9b626ccc8b449f73906484
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-06-30 20:51:50 -0500

    shape abstract class example

--- src/examples/06_module/02_shapes/main.cpp
+++ src/examples/06_module/02_shapes/main.cpp
@@ -11,8 +11,16 @@ Create vector of Shape pointers
 iterate with auto
 */
 
+using std::unique_ptr; using std::make_unique;
+
 int main() 
 {
+	//unique_ptr<Shape> shape = make_unique<Shape>();
+	unique_ptr<Shape> line = make_unique<Line>();
+	unique_ptr<Shape> circle = make_unique<Circle>();
+	//shape->draw();
+	line->draw();
+	circle->draw();
 
 	return 0;
 }
--- src/examples/06_module/02_shapes/shape.h
+++ src/examples/06_module/02_shapes/shape.h
@@ -1,1 +1,14 @@
 //shape.h
+#include<iostream>
+
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape
+{
+public:
+    virtual void draw()=0;//setting it to zero makes it a pure virtual function
+    //and our Shape class is an abstract class    
+};
+
+#endif 

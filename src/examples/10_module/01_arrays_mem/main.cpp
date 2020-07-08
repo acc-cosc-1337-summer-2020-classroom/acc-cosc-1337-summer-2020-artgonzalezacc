2e9f6cec093f2c16b67df9db26c4591a5083bb93
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-07-08 13:54:44 -0500

    arrays 2

--- src/examples/10_module/01_arrays_mem/main.cpp
+++ src/examples/10_module/01_arrays_mem/main.cpp
@@ -5,22 +5,39 @@ using std::cout;
 using std::cin;
 int main() 
 {
+	
 	const int SIZE = 3;
-	std::string name[SIZE];
+	//allocate stack array
+	std::string names[SIZE]{"Joe", "Mary", "John"};
 	int hours[SIZE];
 	double pay_rate[SIZE];
-	
 
+	//access one value
+	cout<<names[0];//display joe to screen
+
+	//add values to array elements	
 	for(int i=0; i < SIZE; ++i)
 	{
-		cout<<"\nEnter name: ";
-		cin>>name[i];
 		cout<<"\Enter hours: ";
 		cin>>hours[i];
 		cout<<"Enter rate: ";
 		cin>>pay_rate[i];
 	}
 
+	//sum totals
+	auto total_salary{0};
+	//display values
+	for(int i=0; i < SIZE; ++i)
+	{
+		cout<<names[i]<<" ";
+		cout<<hours[i]<<" ";
+		cout<<pay_rate[i]<<" ";
+		cout<<hours[i] * pay_rate[i]<<"\n";
+
+		total_salary += hours[i] * pay_rate[i];
+	}
+
+	cout<<total_salary<<"\n";
 	
 
 	/*

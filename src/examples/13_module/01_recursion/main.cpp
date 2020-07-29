0982ea0cb362d3f289349c80840e733cad321f95
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-07-28 21:06:25 -0500

    recursion

--- src/examples/13_module/01_recursion/main.cpp
+++ src/examples/13_module/01_recursion/main.cpp
@@ -2,6 +2,6 @@
 
 int main() 
 {
-		
+	factorial(5);
 	return 0;
 }
--- src/examples/13_module/01_recursion/recursion.cpp
+++ src/examples/13_module/01_recursion/recursion.cpp
@@ -1,6 +1,31 @@
 #include "recursion.h"
 #include<iostream>
 //Write code for recursive display function
+void display(int count)
+{
+    if(count == 0)
+    {
+        return ;
+    }
 
+    std::cout<<"hello\n";
+    display(count -1);
+}
 
 //Write code for recursive factorial
+int factorial(int n)
+{
+    int f;
+    int r;
+
+    if(n == 0)
+    {
+        return 1;
+    }
+
+    std::cout<<"load stack "<<n-1<<"\n";
+    r = factorial(n - 1);//loading function calls to the stack
+    f = n * r;
+    std::cout<<"unload stack n: "<<n<<" r: "<<r<<" f: "<<f<<"\n";
+    return f;
+}
